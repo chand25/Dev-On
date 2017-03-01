7db0a447c12f5d07cf6decbdba408963943e108c
Shantel Moore <moore.shantel@gmail.com>
2017-03-01 10:17:27 -0500

    Just Bit of Me
    Having some fun with React and React Router to make a portfolio page

---
+++ src/components/Projects.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+
+class About extends Component {
+  render() {
+    return (
+
+
+         <div className="Me">
+              <p className="story">
+                Welcome to the Start of a Beautiful New Relationship.  Born and raised in New York City.
+                The diverse multi-ethnic culture of NY has always influenced me from a child.
+                Therefore, my approach to my work before web development has always been to find the
+                voice of a client so that it would be heard.  My projects are a representation of my ability to
+                not stick to a particular design or style.
+              </p>
+         </div>
+
+    );
+  }
+}
+
+export default About;
