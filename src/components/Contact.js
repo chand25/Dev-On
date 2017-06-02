ee54b1b72506e42be52f4e6950b08c1027a696dc
Shantel Moore <moore.shantel@gmail.com>
2017-06-02 02:43:09 -0400

    added side by side background on contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import Contactbkg from '../assets/nyclight.gif';
+import Dreamnyc from '../assets/dreamnyc.jpg';
+import Bkbridgeorange from '../assets/bkbridgeorange.jpg';
+import Rejoice from '../assets/P2R.jpg';
 
 
 class Contact extends Component {
@@ -7,14 +10,23 @@ class Contact extends Component {
   render() {
     let style = {
     backgroundImage: `url(${Contactbkg})` };
+
+    let dreamnyc = {
+    backgroundImage: `url(${Dreamnyc})` };
+
+    let bkbridge = {
+    backgroundImage: `url(${Bkbridgeorange})` };
+    let rejoice = {
+    backgroundImage: `url(${Rejoice})` };
+
     return (
 
       <div className="wrapper">
-          <div className="pagebkg contact" style={style}>
-              <div className="leftcontact">
+          <div className="pagebkg contact">
+              <div className="leftcontact" style={dreamnyc}>
                   <h1 className="contactwords">Contact Me</h1>
               </div>
-              <div className="rightcontact">
+              <div className="rightcontact" style={rejoice}>
                   <h1 className="contactwords">Contact Me</h1>
               </div>
           </div>
