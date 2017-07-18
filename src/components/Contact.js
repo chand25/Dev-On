71bf8e781879b34caed499cf5638d00711da3578
Shantel Moore <moore.shantel@gmail.com>
2017-07-18 00:56:28 -0400

    adding slides to contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,7 +3,10 @@ import Dreamnyc from '../assets/dreamnyc.jpg';
 import Bkbridgeorange from '../assets/bkbridgeorange.jpg';
 import Rejoice from '../assets/P2R.jpg';
 import Expectbt from '../assets/phonemakeup.jpg';
-import Spread from '../assets/phonemail.jpg';
+import ContactWay from '../assets/phonemail.jpg';
+import Card from '../assets/buscard.png';
+import Monitor from '../assets/amonitor.jpeg';
+
 
 
 class Contact extends Component {
@@ -22,19 +25,30 @@ class Contact extends Component {
     let expectBest= {
     backgroundImage: `url(${Expectbt})` };
 
-    let contactWay = {
-    backgroundImage: `url(${Spread})` };
+
 
     return (
 
-      <div className="wrapper">
+      <div className="wrapperr">
           <div className="slider">
-                  <div className="left">
-                      <img src={expectBest} alt="phone screens with developer lingo"/>
-                      <img src={rejoice} alt="girl hands up rejoicing"/>
-                      <img src={dreamnyc} alt="dream in a bottle"/>
+                  <div className="leftcontact">
+                      <img src={Expectbt} alt="phone screens with developer lingo"/>
+                      <img src={ContactWay} alt="pics of mail box and payphone"/>
+                      <img src={Expectbt} alt="phone screens with developer lingo"/>
+                      <img src={ContactWay} alt="pics of mail box and payphone"/>
+                      <img src={Expectbt} alt="phone screens with developer lingo"/>
                   </div>
           </div>
+          <div className="slider">
+                  <div className="rightcontact">
+                      <img src={Monitor} alt="pics of mail box and payphone"/>
+                      <img src={Rejoice} alt="girl hands up rejoicing"/>
+                       <img src={Dreamnyc} alt="dream in a bottle"/>
+                      <img src={Card} alt="woman holding card"/>
+                      <img src={Monitor} alt="pics of mail box and payphone"/>
+                      <img src={Card} alt="woman holding card"/>
+                  </div>
+           </div>
 
       </div>
     );
